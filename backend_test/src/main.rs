4a883d85370b44254cc6a4432b19468b6a1a29b1
Jordan Heinrichs <mail@jordanheinrichs.com>
2023-10-19 15:36:33 -0600

    Add simple test server

---
+++ backend_test/src/main.rs
@@ -0,0 +1,49 @@
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        .route("/hello_world", post(hello_someone))
+        .route("/hello_world", get(hello_world));
+
+    // run our app with hyper, listening globally on port 3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn hello_someone(
+    Json(payload): Json<HelloWorldRequest>,
+) -> (StatusCode, Json<HelloWorldResponse>) {
+    let res = HelloWorldResponse {
+        result: format!("Hello {}", payload.name),
+    };
+    (StatusCode::OK, Json(res))
+}
+
+async fn hello_world() -> (StatusCode, Json<HelloWorldResponse>) {
+    let res = HelloWorldResponse {
+        result: format!("Hello world"),
+    };
+    (StatusCode::OK, Json(res))
+}
+
+#[derive(Deserialize)]
+struct HelloWorldRequest {
+    name: String,
+}
+
+#[derive(Serialize)]
+struct HelloWorldResponse {
+    result: String,
+}
