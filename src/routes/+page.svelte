986753f7662ee4c0745f24fd5e7149d9d8e4f1f9
Jordan Heinrichs <mail@jordanheinrichs.com>
2025-01-12 23:01:17 -0700

    Add a bit of styling to the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,28 +14,32 @@
 </script>
 
 <main class="container">
-  <div class="flex-1 flex-row">
-    <img src="/logo.png" alt="Logo" class="h-20" />
-    <h1 class="text-3xl font-bold text-center p-1">Avi Logbook</h1>
+  <div class="flex flex-auto flex-row items-center bg-base-200 sticky top-0">
+    <img src="/logo.png" alt="Logo" class="size-20 ml-2" />
+    <h1 class="text-3xl font-bold grow text-primary">Avy Logbook</h1>
+    <button class="btn btn-primary mr-4" onclick={() => createLog()}
+      >New Entry</button
+    >
   </div>
 
-  <button class="btn btn-primary" onclick={() => createLog()}>New Entry</button>
-
-  <div class="list">
+  <div class="list bg-base-100 rounded-box shadow-md">
     {#if data.logs.length === 0}
       <div class="list-row">
-        <p>Add entry to get started</p>
+        <p>Add entry to get started.</p>
       </div>
     {:else}
       {#each data.logs as log}
         <!-- svelte-ignore a11y_click_events_have_key_events -->
         <!-- svelte-ignore a11y_no_static_element_interactions -->
         <div
-          class="list-row"
+          class="list-row flex flex-row justify-items-end cursor-pointer items-center hover:bg-primary-content"
           onclick={() => gotoLog(log.id)}
           aria-label="Go to log"
         >
-          <div class="font-semibold">{log.name}</div>
+          <div class="">{log.name}</div>
+          <div class="text-xs uppercase font-semibold opacity-60">
+            {log.date}
+          </div>
           <!-- svelte-ignore a11y_consider_explicit_label -->
           <button class="btn btn-square btn-ghost">
             <svg
